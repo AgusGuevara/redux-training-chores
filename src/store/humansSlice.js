dae2174913b8a81b2cd6e397aa31d3b983e81633
AgusGuevara <agusguevara97@gmail.com>
2022-08-05 16:35:27 -0300

    Connected redux with ui so app is functional

---
+++ src/store/humansSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { tasksSlice } from './tasksSlice';
+
+const createHuman = (name) => ({
+  id: nanoid(),
+  name,
+  taskIds: []
+});
+
+const initialState = [createHuman('agustin'), createHuman('sarasa')];
+
+export const humansSlice = createSlice({
+  name: 'humans',
+  initialState,
+  reducers: {
+    add: (state, action) => {
+      state.push(createHuman(action.payload));
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(tasksSlice.actions.assignToUser, (state, action) => {
+      for (const human of state) {
+        if (human.id === action.payload.humanId) {
+          human.taskIds.push(action.payload.taskId);
+        } else {
+          human.taskIds = human.taskIds.filter(
+            (id) => id !== action.payload.taskIds
+          );
+        }
+      }
+    });
+  }
+});
